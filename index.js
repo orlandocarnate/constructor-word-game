ca65d78ae7413869b4429201d575edd9270f446a
Orlando Carnate <orlando.carnate@gmail.com>
2019-02-17 15:47:46 -0600

    npm install inquirer. test prompts

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 // import Word that is exported from word.js,
 // which imports Letter that is exported from letter.js
 var Word = require("./word");
+var inquirer = require("inquirer");
 
+var word = new Word;
 // randomly choose word and store in Word.letters array
 
 
 // prompt user for each guess and keep track
 // of the remaining guesses
+
+// Prompt the user to provide location information.
+inquirer.prompt([
+
+    {
+        type: "input",
+        name: "userInput",
+        message: "Guess a letter: "
+    }
+
+    // After the prompt, store the user's response in a variable called location.
+]).then(function (char) {
+    console.log("prompt: ", char.userInput);
+    // send input char to Word.checkChar(char)
+    word.checkChar(char.userInput);
+    // console.log("You entered: ", data);
+});
--- word.js
+++ word.js
@@ -1,15 +1,19 @@
 // import Letter that was exported from letter.js
 var Letter = require("./letter");
 
-var Word = function () {
+var Word = function (arg) {
     // array of letter OBJECTS of current word
+    console.log(arg);
+    /*
     this.letters = [{}]; 
     
     this.getChar = function(arg) {
         // call the showChar()
         Letter.showChar(arg);
     }
-    this.checkChar = function(arg) {
+    */
+    this.checkChar = function (arg) {
+        console.log("checkChar:", arg);
         //check each char in word from 
         // Letter.checkChar() using recursion
     }
