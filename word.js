3f9461d5cc1c7341d0e74cf17de3d62e9fbbb504
Orlando Carnate <orlando.carnate@gmail.com>
2019-02-18 14:22:19 -0600

    [word.js] was able to create Letter Objects, each with guessedChar=false.

--- word.js
+++ word.js
@@ -2,22 +2,35 @@
 var Letter = require("./letter");
 
 
-var Word = function (word) {
+var Word = function () {
     // array of letter OBJECTS of current word
-    this.wordArray = word.split("");
-    
+    this.wordObjects = {};
+
+    this.createObjects = function (word) {
+        var tempArray = word.split("");
+        for (var i = 0; i < tempArray.length; i++) {
+            this.wordObjects[i] = new Letter(tempArray[i]);
+            // console.log(tempArray[i]);
+            console.log(this.wordObjects[i].underlyingChar, this.wordObjects.guessChar);
+        }
+        console.log("wordObjects: ", this.wordObjects);
+        return;
+    }
+
     // function that returns a string representing the word
-    this.showString = function() {
+    this.showString = function () {
         // call function on each letter object that displays 
         // a char or underscore and concatenate them together.
         // for each Letter.showChar();
 
     }
     // takes char argument and calls guess function on each letter
-    this.guessChar =  function (char) {
+    this.guessChar = function (char) {
         Letter.checkChar(char);
     }
 };
 
 // export
 module.exports = Word;
+testWord = new Word();
+testWord.createObjects("test");
